80f54909d1ee495022dbfe1bfde74c09f442d05e
AdekWHATT <vyacheslaw.kilin@mail.ru>
2022-01-10 11:28:02 +0300

    Update component About

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,10 +4,10 @@ export const About = () => {
     return (
         <div className="jumbotron">
         <h1 className="display-4">Информация</h1>
-        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p className="lead">Это SPA приложение сделано на React</p>
         <hr className="my-4"/>
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a className="btn btn-primary btn-lg" href="#" role="button">Узнать больше</a>
+        <p>Использовались хуки useState, useEffect, useContext, useReducer и прочие</p>
+        <a className="btn btn-primary btn-lg" href="https://github.com/AdekWHATT" role="button">Моя страница GitHub</a>
       </div>
     )
 }
